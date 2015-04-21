823b24c1dab4d7691d3a0cb0e95492050d3567a8
Jan Mussler <jan.mussler@zalando.de>
2015-04-21 17:17:51 +0200

    Added metrics

--- src/main/java/de/zalando/zmon/dataservice/Application.java
+++ src/main/java/de/zalando/zmon/dataservice/Application.java
@@ -1,12 +1,18 @@
 package de.zalando.zmon.dataservice;
 
+import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 /**
  * Created by jmussler on 4/21/15.
@@ -18,11 +24,27 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan
 public class Application {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    DataServiceMetrics metrics;
+
     @Autowired
     DataServiceConfig config;
 
     @Autowired
     ObjectMapper mapper;
 
+    @RequestMapping(value="/api/v1/data/{checkid}/{accountid}/", method= RequestMethod.PUT, consumes = {"text/plain", "application/json"})
+    void putData(@PathVariable(value="checkid") int checkId, @PathVariable(value="accountid") String accountId, @RequestBody String data) {
+        LOG.info("data: {}", data);
 
+        metrics.markRate();
+        metrics.markAccount(accountId, data.length());
+        metrics.markCheck(checkId, data.length());
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/de/zalando/zmon/dataservice/DataServiceMetrics.java
@@ -0,0 +1,71 @@
+package de.zalando.zmon.dataservice;
+
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by jmussler on 4/21/15.
+ */
+@Component
+public class DataServiceMetrics {
+
+    private final MetricRegistry metrics;
+
+    private final Map<String, Meter> accountMeters = new HashMap<>();
+    private final Map<String, Counter> accountCounter = new HashMap<>();
+
+    private final Map<String, Meter> checkMeters = new HashMap<>();
+    private final Map<String, Counter> checkCounter = new HashMap<>();
+
+    private final Meter totalRate;
+
+    @Autowired
+    public DataServiceMetrics(MetricRegistry metrics) {
+        this.metrics = metrics;
+        this.totalRate = metrics.meter("data-service.total-rate");
+    }
+
+    public Meter getOrCreateMeter(Map<String, Meter> meters, String name) {
+        Meter m = meters.get(name);
+        if(null!=m) return m;
+        synchronized (this) {
+            m = meters.get(name);
+            if(null!=m) return m;
+            m = metrics.meter(name);
+            meters.put(name, m);
+            return m;
+        }
+    }
+
+    public Counter getOrCreateCounter(Map<String, Counter> counters, String name) {
+        Counter m = counters.get(name);
+        if(null!=m) return m;
+        synchronized (this) {
+            m = counters.get(name);
+            if(null!=m) return m;
+            m = metrics.counter(name);
+            counters.put(name, m);
+            return m;
+        }
+    }
+
+    public void markRate() {
+        totalRate.mark();
+    }
+
+    public void markAccount(String account, int size) {
+        getOrCreateMeter(accountMeters, "ds.acc."+account+".rate").mark(size);
+        getOrCreateCounter(accountCounter, "ds.acc."+account+".counter").inc(size);
+    }
+
+    public void markCheck(int checkId, int size) {
+        getOrCreateMeter(checkMeters, "ds.check."+checkId+".rate").mark(size);
+        getOrCreateCounter(checkCounter, "ds.check."+checkId+".counter").inc(size);
+    }
+}
