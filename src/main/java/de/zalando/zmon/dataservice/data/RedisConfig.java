01b594cc0df644d6776d22e0c5f6206d5fe586fe
Joerg Bellmann <joerg.bellmann@zalando.de>
2016-01-25 16:20:54 +0100

    add note about AutoConfiguration

--- src/main/java/de/zalando/zmon/dataservice/data/RedisConfig.java
+++ src/main/java/de/zalando/zmon/dataservice/data/RedisConfig.java
@@ -7,16 +7,18 @@ import de.zalando.zmon.dataservice.config.DataServiceConfigProperties;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+// TODO, if we not depend on JedisPool directly anymore,
+// we can remove this and use RedisAutoConfiguration of Spring-Boot
 @Configuration
 public class RedisConfig {
 
-	@Bean
-	JedisPool getPool(DataServiceConfigProperties config) {
-		JedisPoolConfig poolConfig = new JedisPoolConfig();
-		poolConfig.setTestOnBorrow(true);
-		poolConfig.setMaxTotal(config.getRedisPoolSize());
+    @Bean
+    JedisPool getPool(DataServiceConfigProperties config) {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setMaxTotal(config.getRedisPoolSize());
 
-		return new JedisPool(poolConfig, config.getRedisHost(), config.getRedisPort());
-	}
+        return new JedisPool(poolConfig, config.getRedisHost(), config.getRedisPort());
+    }
 
 }
