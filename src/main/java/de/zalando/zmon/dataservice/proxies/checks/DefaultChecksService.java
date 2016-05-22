e92c2474be34ce612a7223d2a26bf09391faf443
Jan M <janm81@gmail.com>
2016-05-22 11:29:59 +0200

    Fix alert/check proxy to use proper timeouts/settings, also continue with new executor. Future rewrite pending

--- src/main/java/de/zalando/zmon/dataservice/proxies/checks/DefaultChecksService.java
+++ src/main/java/de/zalando/zmon/dataservice/proxies/checks/DefaultChecksService.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
 
 import de.zalando.zmon.dataservice.config.DataServiceConfigProperties;
+import org.apache.http.impl.client.HttpClients;
 
 public class DefaultChecksService implements ChecksService {
 
@@ -17,18 +21,31 @@ public class DefaultChecksService implements ChecksService {
 		this.config = config;
 	}
 
+	public static HttpClient getHttpClient(int socketTimeout, int timeout, int maxConnections) {
+		RequestConfig config = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(timeout).build();
+		return HttpClients.custom().setMaxConnPerRoute(maxConnections).setMaxConnTotal(maxConnections).setDefaultRequestConfig(config).build();
+	}
+
 	@Override
 	public String allActiveAlertDefinitions(String query) throws URISyntaxException, IOException {
+
+		final Executor executor = Executor.newInstance(getHttpClient(100, 5000, 1));
+
 		URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/checks/all-active-alert-definitions")
 				.setParameter("query", query).build();
-		return Request.Get(uri).useExpectContinue().execute().returnContent().asString();
+
+		return executor.execute(Request.Get(uri)).returnContent().asString();
 	}
 
 	@Override
 	public String allActiveCheckDefinitions(String query) throws URISyntaxException, IOException {
+
+		final Executor executor = Executor.newInstance(getHttpClient(100, 5000, 1));
+
 		URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/checks/all-active-check-definitions")
 				.setParameter("query", query).build();
-		return Request.Get(uri).useExpectContinue().execute().returnContent().asString();
+
+		return executor.execute(Request.Get(uri)).returnContent().asString();
 	}
 
 }
