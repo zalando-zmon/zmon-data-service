20673ba4e6d661cbeca1bec137976d0a49b1acfa
Jan M <janm81@gmail.com>
2016-05-22 11:25:48 +0200

    Set connection timeouts in Entities proxy. Use new executor for now every time

--- src/main/java/de/zalando/zmon/dataservice/proxies/entities/DefaultEntitiesService.java
+++ src/main/java/de/zalando/zmon/dataservice/proxies/entities/DefaultEntitiesService.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.utils.URIBuilder;
@@ -15,57 +17,60 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import de.zalando.zmon.dataservice.DataServiceMetrics;
 import de.zalando.zmon.dataservice.components.CustomObjectMapper;
 import de.zalando.zmon.dataservice.config.DataServiceConfigProperties;
+import org.apache.http.impl.client.HttpClients;
 
 /**
- * 
  * @author jbellmann
- *
  */
 public class DefaultEntitiesService implements EntitiesService {
 
-	
-	private final DataServiceConfigProperties config;
-	private final ObjectMapper customObjectMapper;
-	private final DataServiceMetrics metrics;
+    private final DataServiceConfigProperties config;
+    private final ObjectMapper customObjectMapper;
+    private final DataServiceMetrics metrics;
 
-	public DefaultEntitiesService(@CustomObjectMapper ObjectMapper customObjectMapper, DataServiceConfigProperties config, DataServiceMetrics metrics) {
-		this.customObjectMapper = customObjectMapper;
-		this.config = config;
-		this.metrics = metrics;
-	}
+    public DefaultEntitiesService(@CustomObjectMapper ObjectMapper customObjectMapper, DataServiceConfigProperties config, DataServiceMetrics metrics) {
+        this.customObjectMapper = customObjectMapper;
+        this.config = config;
+        this.metrics = metrics;
+    }
 
-	@Override
-	public String deleteEntity(String id) throws URISyntaxException, IOException{
-        URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/entities/"+id+'/').build();
+    public static HttpClient getHttpClient(int socketTimeout, int timeout, int maxConnections) {
+        RequestConfig config = RequestConfig.custom().setSocketTimeout(socketTimeout).setConnectTimeout(timeout).build();
+        return HttpClients.custom().setMaxConnPerRoute(maxConnections).setMaxConnTotal(maxConnections).setDefaultRequestConfig(config).build();
+    }
 
-        final Executor executor = Executor.newInstance();
+    @Override
+    public String deleteEntity(String id) throws URISyntaxException, IOException {
+        URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/entities/" + id + "/").build();
 
+        final Executor executor = Executor.newInstance(getHttpClient(100, 5000, 1));
         return executor.execute(Request.Delete(uri)).returnContent().asString();
-	}
+    }
 
-	@Override
-	public String getEntities(String query) throws URISyntaxException, IOException {
-		URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/entities/").setParameter("query",query).build();
-        final String r = Request.Get(uri).execute().returnContent().asString();
+    @Override
+    public String getEntities(String query) throws URISyntaxException, IOException {
+
+        URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/entities/").setParameter("query", query).build();
+
+        final Executor executor = Executor.newInstance(getHttpClient(100, 5000, 1));
+        return executor.execute(Request.Get(uri)).returnContent().asString();
+    }
+
+    @Override
+    public String addEntities(String entities) throws URISyntaxException, IOException {
+        JsonNode node = customObjectMapper.readTree(entities);
+        if (node.has("infrastructure_account")) {
+            String id = node.get("infrastructure_account").textValue();
+            metrics.markEntity(id, 1);
+        }
+
+        URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/entities/").build();
+
+        final Executor executor = Executor.newInstance(getHttpClient(100, 5000, 1));
+        String r = executor.execute(Request.Put(uri)
+                .bodyString(customObjectMapper.writeValueAsString(node), ContentType.APPLICATION_JSON)).returnContent()
+                .asString();
         return r;
-	}
-
-	@Override
-	public String addEntities(String entities) throws URISyntaxException, IOException {
-		JsonNode node = customObjectMapper.readTree(entities);
-		if (node.has("infrastructure_account")) {
-			String id = node.get("infrastructure_account").textValue();
-			metrics.markEntity(id, 1);
-		}
-
-		URI uri = new URIBuilder().setPath(config.getProxyControllerUrl() + "/entities/").build();
-
-		final Executor executor = Executor.newInstance();
-
-		String r = executor.execute(Request.Put(uri)
-				.bodyString(customObjectMapper.writeValueAsString(node), ContentType.APPLICATION_JSON)).returnContent()
-				.asString();
-		return r;
-	}
+    }
 
 }
