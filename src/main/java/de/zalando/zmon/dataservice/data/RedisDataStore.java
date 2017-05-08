73c5c436ec26c2204f5cb4292e9e83a4a7238a5b
lmineiro <luis.mineiro@zalando.de>
2017-05-08 15:56:34 +0200

    key name was wrong. great catch @mohabusama

--- src/main/java/de/zalando/zmon/dataservice/data/RedisDataStore.java
+++ src/main/java/de/zalando/zmon/dataservice/data/RedisDataStore.java
@@ -138,7 +138,7 @@ public class RedisDataStore {
                         p.hset("zmon:alerts:" + alert.alert_id + ":entities", cd.entity_id, captures);
 
                         p.eval("if table.getn(redis.call('smembers','zmon:alerts:" + alert.alert_id + "')) == 0 then " +
-                                    "redis.call('srem','zmon:alert_acks', " + alert.alert_id + "); " +
+                                    "redis.call('srem','zmon:alert-acks', " + alert.alert_id + "); " +
                                     "return redis.call('srem','zmon:alerts'," + alert.alert_id + ") " +
                                 "else " +
                                     "return redis.call('sadd','zmon:alerts'," + alert.alert_id + ") " +
