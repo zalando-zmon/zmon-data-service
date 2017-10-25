b4ecff1c295c6bacd62b34d09cb99dbc4e2e0a12
Mohab Usama <mohab.abdelhameed@zalando.de>
2017-10-25 19:22:12 +0200

    Skip redis writer

--- src/main/java/de/zalando/zmon/dataservice/data/RedisWorkerResultWriter.java
+++ src/main/java/de/zalando/zmon/dataservice/data/RedisWorkerResultWriter.java
@@ -27,15 +27,7 @@ class RedisWorkerResultWriter implements WorkResultWriter {
     @Override
     public void write(WriteData writeData) {
         log.debug("write to redis ...");
-        if (writeData.getWorkerResultOptional().isPresent()) {
-            try {
-                redisDataStore.store(writeData.getWorkerResultOptional().get());
-                log.debug("written to redis");
-            } catch (Exception e) {
-                log.error("failed redis write check={} data={}", writeData.getCheckId(), writeData.getData(), e);
-                metrics.markRedisError();
-            }
-        }
+        return;
     }
 
 }
