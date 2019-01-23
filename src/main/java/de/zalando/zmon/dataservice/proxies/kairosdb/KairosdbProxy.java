11fd18868ca983417ad97091764fc6da8c8304f6
Alex Korotkykh <me@alexkorotkikh.com>
2019-01-23 14:32:07 +0100

    fix potential NPE

--- src/main/java/de/zalando/zmon/dataservice/proxies/kairosdb/KairosdbProxy.java
+++ src/main/java/de/zalando/zmon/dataservice/proxies/kairosdb/KairosdbProxy.java
@@ -111,7 +111,12 @@ public class KairosdbProxy {
 
 
     private void fixMetricNames(final JsonNode node) {
-        for (final JsonNode metric : node.get("metrics")) {
+        final JsonNode metrics = node.get("metrics");
+        if (metrics == null || !metrics.isArray()) {
+            return;
+        }
+
+        for (final JsonNode metric : metrics) {
             final Optional<JsonNode> tags = Optional.ofNullable(metric.get("tags"));
             final Optional<JsonNode> keyNode = tags.map(t -> t.get("key")).filter(k -> !k.textValue().isEmpty());
             if (keyNode.isPresent()) {
