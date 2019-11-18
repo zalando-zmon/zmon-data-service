824016eec51a33f6d31fe4d7ef332f48648a7ee4
Alex Korotkikh <me@alexkorotkikh.com>
2019-11-18 15:23:47 +0100

    make it possible to sample ingestion by tier (#137)
    * make it possible to sample ingestion by tier
    
    * fix NPE

--- src/test/java/de/zalando/zmon/dataservice/data/MetricTiersTest.java
+++ src/test/java/de/zalando/zmon/dataservice/data/MetricTiersTest.java
@@ -2,47 +2,86 @@ package de.zalando.zmon.dataservice.data;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
 import de.zalando.zmon.dataservice.TokenWrapper;
 import de.zalando.zmon.dataservice.config.DataServiceConfigProperties;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Random;
 
-import static org.hibernate.validator.internal.util.CollectionHelper.newHashSet;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MetricTiersTest {
+    private MetricTiers metricTiers;
+    private Random mockedRandom;
 
-    @Test
-    public void isMetricEnabled() {
-        final MetricTiers metricTiers = new MetricTiers(
+    @Before
+    public void setUp() {
+        mockedRandom = mock(Random.class);
+        metricTiers = new MetricTiers(
                 mock(ObjectMapper.class),
                 mock(DataServiceConfigProperties.class),
-                mock(TokenWrapper.class)
+                mock(TokenWrapper.class),
+                mockedRandom
         );
+    }
 
-        final Set<Integer> set = ImmutableSet.of(1);
-
-        // max tier is not in (critical, important)
+    @Test
+    public void maxTierIsNotCriticalOrImportant() {
         metricTiers.setIngestMaxCheckTier(0);
         assertTrue(metricTiers.isMetricEnabled(1));
         metricTiers.setIngestMaxCheckTier(3);
         assertTrue(metricTiers.isMetricEnabled(1));
+    }
 
-        // max tier = important
+    @Test
+    public void maxTierIsImportant() {
         metricTiers.setIngestMaxCheckTier(2);
         assertFalse(metricTiers.isMetricEnabled(1));
-        metricTiers.setImportantChecks(set);
+        metricTiers.setImportantChecks(ImmutableSet.of(1));
         assertTrue(metricTiers.isMetricEnabled(1));
+    }
 
-        // max tier = critical
+    @Test
+    public void maxTierIsCritical() {
         metricTiers.setIngestMaxCheckTier(1);
         assertFalse(metricTiers.isMetricEnabled(1));
-        metricTiers.setCriticalChecks(set);
+        metricTiers.setCriticalChecks(ImmutableSet.of(1));
+        assertTrue(metricTiers.isMetricEnabled(1));
+    }
+
+    @Test
+    public void maxTierIsOtherAndSampledTierIsOther() {
+        metricTiers.setSampledCheckRate(0.5);
+
+        metricTiers.setIngestMaxCheckTier(3);
+        metricTiers.setSampledCheckTier(3);
+        when(mockedRandom.nextDouble()).thenReturn(0.3);
+        assertFalse(metricTiers.isMetricEnabled(1));
+        when(mockedRandom.nextDouble()).thenReturn(0.7);
+        assertTrue(metricTiers.isMetricEnabled(1));
+    }
+
+    @Test
+    public void maxTierIsImportantAndSampledTierIsOther() {
+        metricTiers.setSampledCheckRate(0.5);
+
+        metricTiers.setIngestMaxCheckTier(2);
+        metricTiers.setImportantChecks(ImmutableSet.of(1));
+        when(mockedRandom.nextDouble()).thenReturn(0.3);
+        assertTrue(metricTiers.isMetricEnabled(1)); // random not called here
+        assertFalse(metricTiers.isMetricEnabled(2));
+    }
+
+    @Test
+    public void criticalChecksAreNotSampled() {
+        metricTiers.setSampledCheckRate(0.5);
+
+        metricTiers.setSampledCheckTier(1);
+        metricTiers.setCriticalChecks(ImmutableSet.of(1));
         assertTrue(metricTiers.isMetricEnabled(1));
     }
 }
